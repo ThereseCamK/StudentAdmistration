b51061a2fb02bef67b9f76862f3920d506f49f92
Therese C Kjær <69575527+ThereseCamK@users.noreply.github.com>
2024-06-18 09:11:34 +0200

    Initial commit

---
+++ StudentAdmistration/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace StudentAdmistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Legg til en student
+            //Vis alle studenter
+            //Beregn gjennomsnittskarakter for alle studenter
+            //Avslutt
+
+            List<Student> students = new List<Student>();
+            Console.WriteLine("Velkommen til stundetoversikten");
+            Console.WriteLine("1. for å legge til student");
+            Console.WriteLine("2. for å vise alle studentene");
+            Console.WriteLine("3. for å bergene grannomsnittskarakterer for alle studentene");
+            Student therese = new Student("therese", 2, [2, 4, 6, 4, 5]);
+            Student linn = new Student("linn", 2, [3, 4, 3, 4, 3]);
+            Student rebekka = new Student("rebekka", 2, [6, 5, 6, 4, 5]);
+            students.Add(therese);
+            students.Add(linn);
+            students.Add(rebekka);
+            var userChoice = Console.ReadLine();
+            int choice = Convert.ToInt32(userChoice);
+            if (choice == 1)
+            {
+                Student.AddStudent(students);
+            }
+            else if (choice == 2)
+            {
+                Student.ShowAllStudents(students);
+
+            }
+            else if (choice == 3)
+            {
+                Student.CalculateAverageGrade(students);
+            }
+            else { Console.WriteLine("ugyldig input"); }
+            
+        }
+    }
+}
---
+++ StudentAdmistration/Student.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAdmistration
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public List<double> Grades { get; set; }
+
+        public Student(string name, int id, List<double> grades)
+        {
+            Name = name;
+            Id = id;
+            Grades = grades;
+        }
+
+        internal static void AddStudent(List<Student> students)
+        {
+            Console.WriteLine("Skriv inn Navn:");
+            string name = Console.ReadLine();
+            Console.WriteLine("skriv inn id");
+            string idInput = Console.ReadLine();
+            int id = Convert.ToInt32(idInput);
+            List<double> grades = new List<double>();
+
+            while (true) {
+                Console.WriteLine("skriv inn en karakter (eller 'a' for å avslutte! ");
+                string gradeInput = Console.ReadLine();
+                if(gradeInput.ToLower() == "a")
+                {
+                    break;
+                }
+                if (double.TryParse(gradeInput, out double grade)) { 
+                        grades.Add(grade);
+                }
+                else
+                {
+                    Console.WriteLine("ugyldig karakter, prøv igjen");
+                }
+                Student student = new Student(name, id, grades);    
+                students.Add(student);
+
+                Console.WriteLine("student lagt til");
+
+             
+            }
+        }
+        internal static void ShowAllStudents(List<Student> students)
+        {
+            if(students.Count == 0)
+            {
+                Console.WriteLine("ingen studenter");
+                return;
+            }
+            foreach(var student in students)
+            {
+                Console.WriteLine($"Navn: {student.Name}, Studentnummer: {student.Id}, Karakterer: {string.Join(", ", student.Grades)}");
+            }
+           
+        }
+
+        internal static void CalculateAverageGrade(List<Student> students)
+        {
+            if (students.Count == 0)
+            {
+                Console.WriteLine("ingen studenter");
+                return;
+            }
+            double total = 0;
+            int totalGrades = 0;
+
+            foreach (var student in students) {
+                total += student.Grades.Sum();
+                totalGrades += student.Grades.Count();
+            }
+            double average = total / totalGrades;
+            Console.WriteLine($"gjennomsnittskarakteren for alle studenter er: {average:F2}");
+        }
+    }
+}
